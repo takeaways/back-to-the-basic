a4fd9e9ed0e519cb14693c20cc794109604c7194
happytalk-gi <>
2020-07-16 18:58:27 +0900

    add complate

--- ch3/main.js
+++ ch3/main.js
@@ -4,7 +4,7 @@
 3. delete : trash icon
 */
 
-const items = document.querySelector("items");
+const items = document.querySelector(".items");
 const input = document.querySelector(".footer__input");
 const addBtn = document.querySelector(".footer__button");
 
@@ -14,10 +14,8 @@ function onAdd(){
     //2. 아이템 만들기
     const item = createItem(text);
     //3. 아이템 추가
+    items.appendChild(item)
     //4. input 내용 reset
-    
-    
-    // items.appendChild
     input.value = "";
     input.focus();
 }
@@ -25,9 +23,31 @@ function createItem(text){
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class','item__row');
 
+    const item = document.createElement('div');
+    item.setAttribute('class','item');
+
+    const itemName = document.createElement('span');
+    itemName.setAttribute('class','item__name');
+    itemName.textContent = text;
+
+    const itemDelete =document.createElement('button');
+    itemDelete.setAttribute('class','item__delete');
+    const icon = document.createElement('i');
+    icon.setAttribute('class','fa fa-trash-o')
+    icon.setAttribute('aria-hidden','true')
+
     const itemDivider = document.createElement('div');
-    itemDivider.setAttribute('class','item__divider')
+    itemDivider.setAttribute('class','item__divider');
+
+    item.appendChild(itemName)
+    itemDelete.appendChild(icon);
+    item.appendChild(itemDelete)
+    itemRow.appendChild(item)
+    itemRow.appendChild(itemDivider);
+    return itemRow;
 }
+
+
 function onDelete(){}
 
 
