1d458d9c844b1e58c5177f1d756cb74374573946
JangGeonil <>
2020-07-18 15:35:49 +0900

    refactorying

--- ch3/main.js
+++ ch3/main.js
@@ -1,9 +1,5 @@
-/*
-1. input
-2. add : enter + plus
-3. delete : trash icon
-*/
-
+const container = document.querySelector('.list');
+let uuid = 0;
 const items = document.querySelector('.items');
 const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
@@ -25,37 +21,31 @@ function onAdd() {
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
-
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
-
-  const itemName = document.createElement('span');
-  itemName.setAttribute('class', 'item__name');
-  itemName.innerText = text;
-
-  const itemDelete = document.createElement('button');
-  itemDelete.setAttribute('class', 'item__delete');
-  itemDelete.innerHTML = "<i class='fa fa-trash-o' aria-hidden='true'></i>";
-  itemDelete.addEventListener('click', () => {
-    items.removeChild(itemRow);
-  });
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item__divider');
-
-  item.appendChild(itemName);
-  item.appendChild(itemDelete);
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  itemRow.setAttribute('data-id', uuid);
+  itemRow.innerHTML = `
+    <div class="item">
+      <span class="item__name">${text}</span>
+      <button class="item__delete">
+        <i class='fa fa-trash-o' aria-hidden='true' data-id=${uuid}></i>
+      </button>
+    </div>
+    <div class="item__divider"></div>
+  `;
+  uuid++;
   return itemRow;
 }
 
-function onDelete() {}
-
-addBtn.addEventListener('click', () => {
+items.addEventListener('click', (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id='${id}']`);
+    toBeDeleted.remove();
+  }
+});
+addBtn.addEventListener('click', (event) => {
   onAdd();
 });
-
-input.addEventListener('keypress', (event) => {
+container.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     onAdd();
   }
