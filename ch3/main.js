cbdbe2204d36b34d65bdbdda70ffec9cc64a74cf
JangGeonil <>
2020-07-18 13:33:45 +0900

    making todo

--- ch3/main.js
+++ ch3/main.js
@@ -4,53 +4,59 @@
 3. delete : trash icon
 */
 
-const items = document.querySelector(".items");
-const input = document.querySelector(".footer__input");
-const addBtn = document.querySelector(".footer__button");
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
 
-function onAdd(){
-    //1. 텍스트 받아오기
-    const text = input.value;
-    //2. 아이템 만들기
-    const item = createItem(text);
-    //3. 아이템 추가
-    items.appendChild(item)
-    //4. input 내용 reset
-    input.value = "";
+function onAdd() {
+  //1. 텍스트 받아오기
+  const text = input.value;
+  if (!text.trim()) {
     input.focus();
+    return;
+  }
+  const item = createItem(text);
+  items.appendChild(item);
+  input.value = '';
+  input.focus();
+
+  item.scrollIntoView({ block: 'center' });
 }
-function createItem(text){
-    const itemRow = document.createElement('li');
-    itemRow.setAttribute('class','item__row');
+function createItem(text) {
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
 
-    const item = document.createElement('div');
-    item.setAttribute('class','item');
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item');
 
-    const itemName = document.createElement('span');
-    itemName.setAttribute('class','item__name');
-    itemName.textContent = text;
+  const itemName = document.createElement('span');
+  itemName.setAttribute('class', 'item__name');
+  itemName.innerText = text;
 
-    const itemDelete =document.createElement('button');
-    itemDelete.setAttribute('class','item__delete');
-    const icon = document.createElement('i');
-    icon.setAttribute('class','fa fa-trash-o')
-    icon.setAttribute('aria-hidden','true')
+  const itemDelete = document.createElement('button');
+  itemDelete.setAttribute('class', 'item__delete');
+  itemDelete.innerHTML = "<i class='fa fa-trash-o' aria-hidden='true'></i>";
+  itemDelete.addEventListener('click', () => {
+    items.removeChild(itemRow);
+  });
+  const itemDivider = document.createElement('div');
+  itemDivider.setAttribute('class', 'item__divider');
 
-    const itemDivider = document.createElement('div');
-    itemDivider.setAttribute('class','item__divider');
-
-    item.appendChild(itemName)
-    itemDelete.appendChild(icon);
-    item.appendChild(itemDelete)
-    itemRow.appendChild(item)
-    itemRow.appendChild(itemDivider);
-    return itemRow;
+  item.appendChild(itemName);
+  item.appendChild(itemDelete);
+  itemRow.appendChild(item);
+  itemRow.appendChild(itemDivider);
+  return itemRow;
 }
 
+function onDelete() {}
 
-function onDelete(){}
+addBtn.addEventListener('click', () => {
+  onAdd();
+});
 
-
-addBtn.addEventListener('click', ()=>{
-    onAdd()
-})
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    onAdd();
+  }
+});
